1439f3853d4fee4e9e6b2278907483705e818a7d
LelikL <korod@mail.ru>
2021-09-13 00:13:05 +0400

    lesson_4

---
+++ utils.py
@@ -0,0 +1,14 @@
+from requests import get, utils
+
+response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+encodings = utils.get_encoding_from_headers(response.headers)
+content = response.content.decode(encoding=encodings)
+
+def currency_rates(code):
+    stroka = content.split('<Valute ID=')
+    for i in stroka:
+        if code.upper() in i:
+            return float(i.replace('/', '').split('<Value>')[-2].replace(',', '.'))
+
+if __name__ == "__main__":
+    print(currency_rates("usd"))
